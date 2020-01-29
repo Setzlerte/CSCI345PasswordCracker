f679889babc931e47b1f5ef0a87f6c008b6c9860
sm_netrist <smainar@netrist.com>
2020-01-29 16:36:14 -0500

    aaaaaaAAAAAAHHHHHH

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import hashlib
-import exrex
 
 rule1 = '([A-Z][a-z]{6}[0-9])'
 rule2 = ''
@@ -7,10 +6,21 @@ rule3 = ''
 rule4 = ''
 rule5 = ''
 
+def openWordList():
+	wl = open('/usr/share/dict/words', 'r')
+	wordlist = wl.readlines()
+	wl.close()
+	return wordlist
+
+"""
+Input: a digit from 0-9, wordlist from /usr/share/dict/words
+Output: a list of seven letter words with the given special number appended at the end
+"""
+def firstRuleWord(specialNum, wordlist):
+	
 
 def main():
-	list(exrex.generate(rule1))
-
+	wordlist = openWordList()
 	print("lkajsdfl")
 
 
