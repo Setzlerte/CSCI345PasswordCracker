eee413a0bde66f3e09e3be9875f12cbc86c4a09c
shainamainar <shainamainar@gmail.com>
2020-01-30 13:51:45 -0500

    aslkjdf

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ def openWordList():
 	wl = open('/usr/share/dict/words', 'r')
 	wordlist = wl.readlines()
 	wl.close()
+	for i in range(len(wordlist)):
+		wordlist[i] = wordlist[i].decode('utf-8')
+		wordlist[i] = wordlist[i].replace('\r', '').replace('\n', '')
+		#print(len(wordlist[i]))
 	return wordlist
 
-"""
-Input: a digit from 0-9, wordlist from /usr/share/dict/words
-Output: a list of seven letter words with the given special number appended at the end
-"""
+
 def hasher(word):
 	return hashlib.sha256(word).hexdigest()
-#def firstRuleWord(specialNum, wordlist):
 	
 
 def main():
@@ -24,6 +24,26 @@ def main():
 	print(hasher("taco"))
 	
 	
+"""
+Input: a digit from 0-9, wordlist from /usr/share/dict/words
+Output: a list of seven letter words with the given special number appended at the end
+"""
+def firstRuleWord(specialNum, wordlist):
+	newlist = []
+	for i in range(len(wordlist)):
+		#print(wordlist[i] + " " + str(len(wordlist[i])))
+		if(len(wordlist[i]) == 7):
+			newlist.append(wordlist[i] + str(specialNum))
+	return newlist
+			
+
+def main():
+	wordlist = openWordList()
+	flist = firstRuleWord(1, wordlist)
+	for word in flist:
+		print(word)
+
+
 
 
 
